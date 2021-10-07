cf0aa4d48313f494c9a5d36c1421e4f3e4972dc2
bunchenko <bunchenko@rit.va>
2021-10-07 09:35:30 +0300

    update gatsby-source-filesystem path to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
+        path: `${__dirname}/content`,
+        name: `posts`,
       },
     },
     {
