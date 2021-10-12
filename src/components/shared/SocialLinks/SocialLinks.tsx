d1338458e39db1be774a9a092cd70336ac4bcd12
bunchenko <bunchenko@rit.va>
2021-10-12 16:58:52 +0300

    proceeding with 'about me' page

--- src/components/shared/SocialLinks/SocialLinks.tsx
+++ src/components/shared/SocialLinks/SocialLinks.tsx
@@ -7,6 +7,7 @@ import github from '../../../images/github-social.png';
 import instagram from '../../../images/instagram-social.png';
 import medium from '../../../images/medium-social.png';
 import reddit from '../../../images/reddit-social.png';
+import upwork from '../../../images/upwork-social.png';
 
 import * as styles from './SocialLinks.module.less';
 
@@ -36,49 +37,61 @@ export const SocialLinks = () => {
   return (
     <>
       <h3>Social</h3>
-
-      <a
-        href={`https://twitter.com/${social.twitter}`}
-        className={styles.socialLink}
-      >
-        <img src={twitter} alt="twitter-social" />
-      </a>
-      <a
-        href={`https://www.linkedin.com/in/${social.linkedIn}`}
-        className={styles.socialLink}
-      >
-        <img src={linkedin} alt="linkedin-social" />
-      </a>
-      <a
-        href={`https://www.upwork.com/freelancers/${social.upWork}`}
-        className={styles.socialLink}
-      >
-        <img src={twitter} alt="twitter-social" />
-      </a>
-      <a
-        href={`https://medium.com/${social.medium}`}
-        className={styles.socialLink}
-      >
-        <img src={medium} alt="medium-social" />
-      </a>
-      <a
-        href={`https://www.instagram.com/${social.instagram}`}
-        className={styles.socialLink}
-      >
-        <img src={instagram} alt="instagram-social" />
-      </a>
-      <a
-        href={`https://www.reddit.com/user/${social.reddit}`}
-        className={styles.socialLink}
-      >
-        <img src={reddit} alt="reddit-social" />
-      </a>
-      <a
-        href={`https://github.com/${social.gitHub}`}
-        className={styles.socialLink}
-      >
-        <img src={github} alt="github-social" />
-      </a>
+      <div className={styles.socialLinksWrapper}>
+        <p>
+          Feel free to reach me out. I am always happy to cooperate and
+          communicate on any topics! 👋
+        </p>
+        <a
+          href={`https://twitter.com/${social.twitter}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={twitter} alt="twitter-social" />
+        </a>
+        <a
+          href={`https://www.linkedin.com/in/${social.linkedIn}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={linkedin} alt="linkedin-social" />
+        </a>
+        <a
+          href={`https://www.upwork.com/freelancers/${social.upWork}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={upwork} alt="upwork-social" />
+        </a>
+        <a
+          href={`https://medium.com/${social.medium}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={medium} alt="medium-social" />
+        </a>
+        <a
+          href={`https://www.instagram.com/${social.instagram}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={instagram} alt="instagram-social" />
+        </a>
+        <a
+          href={`https://www.reddit.com/user/${social.reddit}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={reddit} alt="reddit-social" />
+        </a>
+        <a
+          href={`https://github.com/${social.gitHub}`}
+          className={styles.socialLink}
+          target="_blank"
+        >
+          <img src={github} alt="github-social" />
+        </a>
+      </div>
     </>
   );
 };
--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,4 +1,5 @@
 import { graphql, PageProps } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import { Layout } from '../components/Layout';
@@ -14,32 +15,61 @@ const Me: React.FC<PageProps> = ({ data, location }) => {
     <Layout location={location}>
       <Seo title="About Me" />
 
-      {/* <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={['auto', 'webp', 'avif']}
-        src="../images/profile-pic.jpeg"
-        width={80}
-        height={80}
-        quality={95}
-        alt="Profile picture"
-      /> */}
-      {/* {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
-      )} */}
       <article className="blog-page">
         <header>
           <div className={styles.container}>
-            <h1>About me</h1>
-            <p className="description">
-              Software engineer, some other stuff etc blah-blah
+            <h1 className={styles.title}>About me</h1>
+            <p className={styles.subTitle}>Software Engineer</p>
+            <p className={styles.description}>
+              A software engineer from Belarus. I have been working on a
+              large-scale software products over 5 years.
             </p>
+            <StaticImage
+              className="bio-avatar"
+              formats={['auto', 'webp', 'avif']}
+              src="../images/me.jpeg"
+              // layout="fixed"
+              // width={280}
+              // height={280}
+              alt="Profile picture"
+            />
+            <p className={styles.description}>
+              I have worked in cross-functional and multi-cultural teams, which
+              members are from England, Finland, Germany and Pakistan. Have a
+              huge amount of experience teaching the newcomers and junior
+              programmers. I enjoy learning something new, overcoming hardships,
+              co-operating with team members.
+            </p>
+            <p className={styles.description}>
+              As of my non-programming life, I enjoy sports,{' '}
+              <a
+                href="https://www.pinterest.ca/awesomedevdev/potential-tattoos/"
+                target="_blank"
+              >
+                tattoos
+              </a>
+              , cars,{' '}
+              <a
+                href="https://open.spotify.com/playlist/2ijZQmb57z57bL6YAcNvXq?si=3094a54885e44f8e"
+                target="_blank"
+              >
+                music
+              </a>{' '}
+              and art.
+            </p>
+            <p className={styles.description}>Ford Mustang owner 😎</p>
+            <StaticImage
+              className="bio-avatar"
+              formats={['auto', 'webp', 'avif']}
+              src="../images/my-mustang.jpeg"
+              alt="Ford Mustang"
+            />
+            {/* Primary skill - UI/UX. English level: C1. Hard Skills: JavaScript,
+              TypeScript, GraphQL Frameworks: React, VueJS, Angular 8+, Redux,
+              Redux-Observables, Apollo. Other: CSS3, HTML5, RxJS, NodeJS,
+              Material-UI, Flexbox, CSS Grid, Webpack, Gulp, React-Router,
+              Redux-persist, NgRx Adaptive layout, Photoshop, design patterns,
+              code quality, CI/CD */}
           </div>
         </header>
 
